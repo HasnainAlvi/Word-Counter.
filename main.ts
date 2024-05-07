22c40ecbe59ddce461df7138e0fb71ad70bd0d37
hasnain haider alvi <160486646+Hasnainalvi@users.noreply.github.com>
2024-05-08 00:18:16 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+import chalk from "chalk";
+
+const answers: {
+    sentence: String
+} = await inquirer.prompt(
+    {
+        name: "sentence",
+        type: "input",
+        message: `${chalk.bgGray.bold.italic("Enter your sentence to count words: ")}`
+    }
+);
+
+const words = answers.sentence.trim().split(" ");
+
+
+console.log(words);
+
+console.log (`${chalk.bgGray.bold.italic(`Your sentence has ${words.length} words.`)}`);
+
+console.log(`${chalk.bgGray.bold.underline.italic(`THANK YOU FOR USING HASNAIN ALVI WORD COUNTER APP`)}`);
